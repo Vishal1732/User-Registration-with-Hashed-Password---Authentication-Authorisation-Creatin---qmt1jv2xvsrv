741f63645de011ba1cc23c6d54f283c0228bb62b
Vishal Dubey <92910196+Vishal1732@users.noreply.github.com>
2023-05-13 23:42:55 +0530

    Update userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,9 +1,12 @@
 const users   =require("../models/user.js");
 const bcrypt = require('bcrypt');
 
+//to look the user schema look ../models/user.js
+
 const registerUser =async (req, res) => {
 
-    const name = req.body.name;
+    //Write you code here
+     const name = req.body.name;
     const email  = req.body.email;
     const password = req.body.password;
     const DOB = req.body.DOB;
@@ -13,24 +16,18 @@ const registerUser =async (req, res) => {
     const saltRounds = 10;
     const salt = bcrypt.genSaltSync(saltRounds);
     const hashedPassword = bcrypt.hashSync(password, salt);
-
-
     var newuser = {
         "name":name,
         "email":email,
         "password": hashedPassword,
         "DOB":DOB
     };
-
-    // console.log(newuser);
-
     users.create(newuser).then((user) => {
         res.send(user._id);
     })
     .catch((error) => {
         res.status(404).send(error.message);
     });
-
 }
 
 module.exports = { registerUser };
